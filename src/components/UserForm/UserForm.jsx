6708c57be2a36b2a4bee213f442a0fd312f2efc8
nachbr91 <nabero91@gmail.com>
2022-02-09 20:23:04 +0100

    Add ErrorModal component and logic on UserForm

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
 import Button from '../UI/Button'
+import ErrorModal from '../UI/ErrorModal'
 import styles from './UserForm.module.css'
 
 const UserForm = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('')
   const [enteredAge, setEnteredAge] = useState('')
+  const [error, setError] = useState()
 
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value)
@@ -17,13 +19,17 @@ const UserForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault()
-
     if (enteredUsername.trim().length === 0 || enteredAge.length === 0) {
-      setEnteredUsername('')
-      setEnteredAge('')
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid username and age (non-empty values).',
+      })
       return
     } else if (enteredAge < 0) {
-      setEnteredAge('')
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (>0)',
+      })
       return
     }
 
@@ -37,24 +43,37 @@ const UserForm = (props) => {
     setEnteredAge('')
   }
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
-    <form className={styles['user-form']} onSubmit={submitHandler}>
-      <label htmlFor="username">Username</label>
-      <input
-        id="username"
-        type="text"
-        value={enteredUsername}
-        onChange={usernameChangeHandler}
-      />
-      <label htmlFor="age">Age (Years)</label>
-      <input
-        id="age"
-        type="number"
-        value={enteredAge}
-        onChange={ageChangeHandler}
-      />
-      <Button type="submit">Add User</Button>
-    </form>
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <form className={styles['user-form']} onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </>
   )
 }
 
