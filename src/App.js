529148c0bb673682dc3f93585ce119a0d56fec3a
nachbr91 <nabero91@gmail.com>
2022-02-09 01:30:00 +0100

    Edit UserForm adding button and set logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react'
+
 import UserForm from './components/UserForm/UserForm'
+// import UserList from './components/UserList/UserList'
 
 function App() {
+  const [userData, setUserData] = useState([])
+
+  const addUserData = (data) => {
+    setUserData(data)
+  }
+  console.log(userData)
+
   return (
     <div>
-      <UserForm />
+      <UserForm onSaveUserData={addUserData} />
+      {/* <UserList items={userData} /> */}
     </div>
   )
 }
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,16 +1,37 @@
-// import { useState } from 'react'
-import Button from '../UI/Button'
+import { useState } from 'react'
 
 import styles from './UserForm.module.css'
 
-const UserForm = () => {
+const UserForm = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('')
+  const [enteredAge, setEnteredAge] = useState('')
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value)
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const userData = {
+      username: enteredUsername,
+      age: enteredAge,
+    }
+    props.onSaveUserData(userData)
+    setEnteredUsername('')
+    setEnteredAge('')
+  }
+
   return (
-    <form className={styles['user-form']}>
+    <form className={styles['user-form']} onSubmit={submitHandler}>
       <label>Username</label>
-      <input type="text" />
+      <input type="text" onChange={usernameChangeHandler} />
       <label>Age (Years)</label>
-      <input type="number" />
-      <Button />
+      <input type="number" onChange={ageChangeHandler} />
+      <button type="submit">Add User</button>
     </form>
   )
 }
